dfe3e420d78c192fc29cc52e6aa44c0a81bd47fe
Chris Floyd <christopherfloyd16@gmail.com>
2019-11-02 20:11:26 -0400

    added some variables

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,9 @@
 // Draw some text to the screen: =====CODECADEMY=====
-drawName('Happy Birthday!');
+var message = 'Happy Birthday!'
+mouseResponseThreshhold = 0.75; //
+friction = 0.80; //how strongly dots will return to their position 0 - 1
+rotationForce = 0; //how much dots will rotate when interacting 
+
+drawName(message);
 // Animate the text!
 bounceBubbles();
